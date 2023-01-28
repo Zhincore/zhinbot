d393a7fae02b2e036babb12687fc65b0416c4130
Adam Žingor <adam@zhincore.eu>
2023-01-29 00:35:22 +0100

    Fix removing of roles

--- src/modules/selfroles/SelfRoles.discord.ts
+++ src/modules/selfroles/SelfRoles.discord.ts
@@ -77,7 +77,7 @@ export class SelfRolesDiscordAdapter {
   async assignRoles(interaction: Discord.StringSelectMenuInteraction<"cached">, t: TranslateFn) {
     const unselectedRoles = interaction.component.options
       .map((v) => v.value)
-      .filter((v) => interaction.values.includes(v));
+      .filter((v) => !interaction.values.includes(v));
 
     await interaction.member.roles.add(interaction.values, "Self-roles");
     await interaction.member.roles.remove(unselectedRoles, "Self-roles");
